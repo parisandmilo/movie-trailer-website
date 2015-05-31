5535a656e185d03e2fe5597de9e0f00878bae1e6
root <root@ip-10-47-174-141.ec2.internal>
2015-05-31 22:41:44 +0000

    Committed by Udacity

---
+++ fresh_tomatoes.py
@@ -0,0 +1,201 @@
+import webbrowser
+import os
+import re
+
+# Styles and scripting for the page
+main_page_head = '''
+<head>
+    <meta charset="utf-8">
+    <title>Diana's Favourite Movies</title>
+
+    <!-- Bootstrap 3 -->
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <style type="text/css" media="screen">
+        * {
+        font-family: 'Oswald', sans-serif;
+        }
+        body {
+            padding-top: 80px;
+            background: #f0f0f0 url("prettyteapic.jpg") center top;
+        }
+
+        #trailer .modal-dialog {
+            margin-top: 200px;
+            width: 640px;
+            height: 480px;
+        }
+        .hanging-close {
+            position: absolute;
+            top: -12px;
+            right: -12px;
+            z-index: 9001;
+        }
+        #trailer-video {
+            width: 100%;
+            height: 100%;
+        }
+        .text {
+            text-align: center;
+            font-weight: 300;
+            padding: 10px;
+            margin: 5px 25%;
+        }
+        .text:hover {
+            background-color: #FDEADF;
+            cursor: pointer;
+        }
+        .movie-tile {
+            margin-bottom: 40px;
+            padding-top: 20px;
+        }
+        .movie-tile:hover {
+            background-color: #FDEADF;
+            cursor: pointer;
+        }
+        .scale-media {
+            padding-bottom: 56.25%;
+            position: relative;
+        }
+        .scale-media iframe {
+            border: none;
+            height: 100%;
+            position: absolute;
+            width: 100%;
+            left: 0;
+            top: 0;
+            background-color: white;
+        }
+
+        .light {
+            font-weight: 300;
+        }
+    </style>
+    <script type="text/javascript" charset="utf-8">
+        // Pause the video when the modal is closed
+        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
+            // Remove the src so the player itself gets removed, as this is the only
+            // reliable way to ensure the video stops playing in IE
+            $("#trailer-video-container").empty();
+        });
+        // Start playing the video whenever the trailer modal is opened
+        $(document).on('click', '.movie-tile', function (event) {
+            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
+              'id': 'trailer-video',
+              'type': 'text-html',
+              'src': sourceUrl,
+              'frameborder': 0
+            }));
+        });
+        // Animate in the movies when the page loads
+        $(document).ready(function () {
+          $('.movie-tile').hide().first().show("fast", function showNext() {
+            $(this).next("div").show("fast", showNext);
+          });
+        });
+    </script>
+</head>
+'''
+
+# The main page layout and title bar
+main_page_content = '''
+<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <!-- Trailer Video Modal -->
+    <div class="modal" id="trailer">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
+            <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
+          </a>
+          <div class="scale-media" id="trailer-video-container">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <!-- Main Page Content -->
+    <div class="container">
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <!--mobile toggle button :) -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand light" href="#">
+                <img alt="cute tomato" title="image from Adventure-Gal's DeviantArt" src="tomato.png" style="display:inline-block; height:30px; padding-bottom: 5px;"/> DIANA'S FAVOURITE MOVIES
+            </a>
+
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right light">
+                <li><a href="https://github.com/parisandmilo">Back to my Github page  &#9786;</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <div class="container">
+    <p class="text">SCROLL OVER PLEASE! <br/> Hi, this is my page of favourite movies &#9786; <br/> The cute tomato is from <a href="http://fav.me/d66iqmi" target="_blank">here</a>, and if you scroll-over the movie images, you'll see my favourite parts from them! Yay!</p>
+      {movie_tiles}
+    </div>
+  </body>
+</html>
+'''
+
+# A single movie entry html template
+movie_tile_content = '''
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+    <img src="{poster_image_url}" title="{favourite_part}" width="220" height="342">
+    <h2>{movie_title}</h2>
+    <p class="light"><em>{movie_storyline}</em></p>
+    <p><strong>Notable Actors:</strong></p>
+    <p class="light">{notable_actors}</p>
+</div>
+'''
+
+def create_movie_tiles_content(movies):
+    # The HTML content for this section of the page
+    content = ''
+    for movie in movies:
+        # Extract the youtube ID from the url
+        youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+        trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
+
+        # Append the tile for the movie with its content filled in
+        content += movie_tile_content.format(
+            movie_title=movie.title,
+            movie_storyline=movie.storyline,
+            favourite_part = movie.favourite_part,
+            notable_actors=movie.actors,
+            poster_image_url=movie.poster_image_url,
+            trailer_youtube_id=trailer_youtube_id
+        )
+    return content
+
+def open_movies_page(movies):
+  # Create or overwrite the output file
+  output_file = open('fresh_tomatoes.html', 'w')
+
+  # Replace the placeholder for the movie tiles with the actual dynamically generated content
+  rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
+
+  # Output the file
+  output_file.write(main_page_head + rendered_content)
+  output_file.close()
+
+  # open the output file in the browser
+  url = os.path.abspath(output_file.name)
+  webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
---
+++ media.py
@@ -0,0 +1,19 @@
+__author__ = 'dklhy'
+
+import webbrowser
+
+class Movie():
+    """Store movie info"""
+
+    VALID_RATINGS = ["G","PG", "PG-13","NC-16","M-18","R","Unrated"]
+
+    def __init__(self, movie_title, movie_storyline, favourite_part, notable_actors, poster_url, trailer_url):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.favourite_part = favourite_part
+        self.actors = notable_actors
+        self.poster_image_url = poster_url
+        self.trailer_youtube_url = trailer_url
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_url)
